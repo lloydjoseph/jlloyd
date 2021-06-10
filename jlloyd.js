9769a8c5b2745c1779da5420e61a012a814fd698
Joseph Lloyd <lloyd.joseph89@gmail.com>
2021-06-10 19:15:15 +0200

    new navbar and layout

---
+++ jlloyd.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.querySelector('#navbar-open').style.display = 'block';
+}
+
+function closeNavbar() {
+    document.querySelector('#navbar-open').style.display = 'none';
+}
