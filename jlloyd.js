6d3ca48c5f7cd5335d6ef96fd6cf23585694f9c5
Joseph Lloyd <lloyd.joseph89@gmail.com>
2021-06-16 21:51:45 +0200

    cleaned up code and added comments

--- jlloyd.js
+++ jlloyd.js
@@ -1,11 +1,14 @@
+// Opens the navbar on mobile
 function openNavbar() {
     document.querySelector('#navbar-open').style.display = 'block';
 }
 
+// Closes the navbar on mobile
 function closeNavbar() {
     document.querySelector('#navbar-open').style.display = 'none';
 }
 
+// Closes the navbar when a link is clicked
 navbarLinks = document.querySelectorAll('.navbar-link');
 for (let i = 0; i < navbarLinks.length; i++) {
     navbarLinks[i].addEventListener('click', () => {
